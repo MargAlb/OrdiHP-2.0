be013cbb75c0464de0cb84bd53ce0cf5873257fd
Albert Margreiter <a.margreiter@kundl.at>
2025-01-25 23:29:57 +0100

    OrdiHP-2.1.2

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     // }
     // elementResize();        
     // Konstanten Deklarationen:
-    const oeffnungszeit = "urlaub";        // "oeffnungszeit kann sein: urlaub, geschlossen oder offen"
+    const oeffnungszeit = "offen";        // "oeffnungszeit kann sein: urlaub, geschlossen oder offen"
     //const geschlossen = "off";  //closed ODER off
     const zusatz_info = "aus"           //info=ein: Zusatz-Info wird angezeigt
     // info=aus     Zusatz-Info wird nicht angezeigt
